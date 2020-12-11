030a358aef748c809ecb0d33a861251397334d6e
Linlin Ge <linlinge.vip@gmail.com>
2020-12-11 12:09:39 +0800

    Update Statistics.cpp

--- glib/Statistics.cpp
+++ glib/Statistics.cpp
@@ -26,3 +26,15 @@ Statistics::Statistics(vector<float> dat)
 	stdevp_=sqrt(sum/dat.size());
 	stdev_=sqrt(sum/(dat.size()-1));
 }
+double Quantile(vector<int>& dat,double ratio)
+{
+	vector<int> dat2;
+	for(int i=0;i<dat.size();i++) dat2.push_back(dat[i]);
+	sort(dat2.begin(),dat2.end());
+
+	double Q_idx=(dat.size()+1)*ratio-1;
+    int Q_idx_integer=(int)Q_idx;
+    double Q_idx_decimal=Q_idx-Q_idx_integer;
+    double Q=dat2[Q_idx_integer]+(dat2[Q_idx_integer+1]-dat2[Q_idx_integer])*Q_idx_decimal;    
+    return Q;
+}
